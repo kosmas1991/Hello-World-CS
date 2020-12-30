8854e085f870ce1fc6e0f77efd28fa7093655dca
kosmas1991 <kosmasgou@gmail.com>
2020-12-30 21:21:29 +0200

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("To onoma sou einai " + name);
+            Console.WriteLine("poso krono eisai?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Eisai " + age + " kronwn!!!");
+        }
+    }
+}
